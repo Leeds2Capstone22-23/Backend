06347825b1d9a40deeae203e850ee163f0ce1dd0
Abe Handler <akh2103@gmail.com>
2022-11-22 17:14:41 -0700

    fixes merge confict, this is just a copy from mainline

--- demos/demo_python_client.py
+++ demos/demo_python_client.py
@@ -1,19 +1,4 @@
 from sgqlc.endpoint.http import HTTPEndpoint
-<<<<<<< HEAD
-
-# for local development, get the POST endpoint from the Hasura interface
-url = 'http://localhost:8080/v1/graphql'
-headers = {}
-
-# For local development, first add labels using "Add mutation" in the interface
-query = 'query { labels {id} }'
-variables = {}
-
-endpoint = HTTPEndpoint(url, headers)
-data = endpoint(query, variables)
-
-print(data)
-=======
 import base64
 
 
@@ -91,4 +76,3 @@ query {
 data = endpoint(query)
 print('\nSelect labels for user:')
 print(data)
->>>>>>> mainline
